39323233e6d2192d86383c211c8bd6718a2d337b
Sara Walker <sarawall1187@gmail.com>
2018-12-17 23:59:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,8 +37,8 @@ def adding
   # return the shipping_manifest hash below
 
 
-shipping_manifest["oil paintings"]
+
+shipping_manifest 
+endshipping_manifest["oil paintings"]
 shipping_manifest["muskets"]= 2 
 shipping_manifest["gun powder"]= 4
-shipping_manifest 
-end
