894c29ecd83b4b5aae01d1f02efd54f642191db0
Sara Walker <sarawall1187@gmail.com>
2018-12-18 00:01:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+shipping_manifest["oil paintings"]
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -33,7 +33,7 @@ def adding
   # add 4 gun powder to the shipping_manifest hash below
 
 
-shipping_manifest["oil paintings"]
+
   # return the shipping_manifest hash below
 shipping_manifest["muskets"]= 2 
 shipping_manifest["gun powder"]= 4
