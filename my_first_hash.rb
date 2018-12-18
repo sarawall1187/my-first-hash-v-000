b5f8fed157a31ab2716e22b14608f35943c3edcf
Sara Walker <sarawall1187@gmail.com>
2018-12-18 00:00:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,14 +35,14 @@ def adding
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest["muskets"]= 2 
+shipping_manifest["gun powder"]=
 
 
 shipping_manifest 
 end
 
 
-shipping_manifest["muskets"]= 2 
-shipping_manifest["gun powder"]= 4
+ 4
 shipping_manifest["oil paintings"]
 
